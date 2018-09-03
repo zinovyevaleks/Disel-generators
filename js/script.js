b76b44b54a214f877beda25be7ec585de338ff60
zinovyevaleks <zinovyevaleks@gmail.com>
2018-09-03 21:05:01 +0700

    Добавил фильтрацию
    Теперь можно фильтровать каталог по двум параметрам (категории и тип)

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(function(){
+    $("#Container").mixItUp({
+          
+        //   selectors: {
+        //       target:'.mix',
+        //       filter:'.filter',
+        //       sort:'.sort'
+        //   },
+        //   load: {
+        //       sort:'order:desc'
+        //   },
+          
+        //   controls: {
+        //       enable:true,
+        //       activeClass:'on'
+        //   },
+          
+        //   animation: {
+        //       enable:true,
+        //       effects:'scale fade',
+        //       duration:200
+        //   },        
+    });
+  });

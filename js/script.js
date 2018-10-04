758b1c608669a37f3aadef557fbb7621275e89f8
zinovyevaleks <zinovyevaleks@gmail.com>
2018-10-04 11:57:58 +0700

    почистил от лишнего текста

--- js/script.js
+++ js/script.js
@@ -1,24 +1,5 @@
 $(function(){
     $("#Container").mixItUp({
-          
-        //   selectors: {
-        //       target:'.mix',
-        //       filter:'.filter',
-        //       sort:'.sort'
-        //   },
-        //   load: {
-        //       sort:'order:desc'
-        //   },
-          
-        //   controls: {
-        //       enable:true,
-        //       activeClass:'on'
-        //   },
-          
-        //   animation: {
-        //       enable:true,
-        //       effects:'scale fade',
-        //       duration:200
-        //   },        
+               
     });
   });
